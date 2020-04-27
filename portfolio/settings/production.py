ec64594ecdc0e355c4ca95ca59062749e9cc76f9
Duncan Maina <duncanmuiru513@gmail.com>
2020-04-27 08:30:01 +0300

    Adjusted Email template

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['dun-can.herokuapp.com']
 
