1077016dc7c0615c400ec05a3499d9bcbc7aa2e7
Duncan Maina <duncanmuiru513@gmail.com>
2020-10-04 11:11:34 +0300

    Removed HTTPS config

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -108,15 +108,15 @@ EMAIL_USE_TLS = True
 
 ADMINS = [('Duncan Maina', os.environ.get('EMAIL_HOST_USER'))]
 
-CORS_REPLACE_HTTPS_REFERER      = True
-HOST_SCHEME                     = "https://"
-SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT             = True
-SESSION_COOKIE_SECURE           = True
-CSRF_COOKIE_SECURE              = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
-SECURE_HSTS_SECONDS             = 1000000
-SECURE_FRAME_DENY               = True
+# CORS_REPLACE_HTTPS_REFERER      = True
+# HOST_SCHEME                     = "https://"
+# SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT             = True
+# SESSION_COOKIE_SECURE           = True
+# CSRF_COOKIE_SECURE              = True
+# SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+# SECURE_HSTS_SECONDS             = 1000000
+# SECURE_FRAME_DENY               = True
 
 django_heroku.settings(locals())
 DATABASES['default'] = dj_database_url.config(conn_max_age=30, ssl_require=True)
